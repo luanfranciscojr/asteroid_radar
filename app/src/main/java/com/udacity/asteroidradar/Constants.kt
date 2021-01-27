0b59d4ae6ab6b8c321a547d7ab9fb0aaf90add30
luan francisco lima fernandes <luanfranciscojr@gmail.com>
2021-01-26 20:39:20 -0400

    Update Constants.kt

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -5,5 +5,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = ""
+    const val API_KEY = "mjn3wkJIPpnQQ5DxStTVaGhoJuGpvf5G1xFUnCHY"
 }
